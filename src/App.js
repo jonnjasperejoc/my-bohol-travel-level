e442d967f7a4af1d29647fc83a262a434c84c76a
Jonn Jasper Ejoc <extern.jejoc@adnoc.ae>
2023-04-24 07:11:15 +0800

    Changed the 404 page to the homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import BoholPage from './pages/BoholPage';
-import NoPage from "./pages/NoPage";
+// import NoPage from "./pages/NoPage";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<BoholPage />} />
           <Route path="/my-bohol-travel-level" element={<BoholPage />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="*" element={<BoholPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
