780a314cb1d9b29c1389761b0283bffa0bae7b0f
Jonn Jasper Ejoc <extern.jejoc@adnoc.ae>
2023-04-24 09:09:00 +0800

    Sepuko

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import BoholPage from './pages/BoholPage';
-// import NoPage from "./pages/NoPage";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <div className="App">
-      <div id="ezoic-pub-ad-placeholder-110"> </div>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<BoholPage />} />
           <Route path="/my-bohol-travel-level" element={<BoholPage />} />
-          <Route path="*" element={<BoholPage />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
